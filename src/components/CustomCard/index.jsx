daf86ec14a407cd62551e01e839dbde7b49d2867
casanavep <casanavep@gmail.com>
2021-11-14 09:24:30 -0600

    search

--- src/components/CustomCard/index.jsx
+++ src/components/CustomCard/index.jsx
@@ -1,80 +1,81 @@
 import {
-    Card,
-    CardMedia,
-    CardContent,
-    CardActions,
-    Typography,
-    CardActionArea,
-    Button,
-  } from "@material-ui/core";
-  import { ShoppingCart } from "@material-ui/icons";
-  import "./style.css";
-  
-  const CustomCard = ({
-    basket,
-    product,
-    // addProduct,
-    // updateProduct,
-    // RemoveItemFromBasket,
-  }) => {
-    return (
-      <Card className="custom-card">
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            alt={product.image}
-            height="260"
-            className="card-image"
-            image={product.image}
-            title={product.title}
-          />
-          <CardContent className="content">
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  CardActionArea,
+  Button,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+import "./style.css";
+
+const CustomCard = ({
+  basket,
+  product,
+  // addProduct,
+  // updateProduct,
+  // RemoveItemFromBasket,
+}) => {
+  return (
+    <Card className="custom-card">
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt={product.image}
+          height="260"
+          className="card-image"
+          image={product.image}
+          title={product.title}
+          platform={product.platform}
+        />
+        <CardContent className="content">
+          <Typography
+            className="title"
+            gutterBottom
+            variant="h5"
+            component="h2"
+          >
+            {product.title}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      {basket && (
+        <CardActions>
+          <Typography
+            className="basket-item-price"
+            gutterBottom
+            variant="h5"
+            component="h2"
+          >
+            {product.price}
+          </Typography>
+        </CardActions>
+      )}
+      <CardActions className="actions-content">
+        {!basket && (
+          <>
             <Typography
-              className="title"
-              gutterBottom
-              variant="h5"
-              component="h2"
-            >
-              {product.title}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        {basket && (
-          <CardActions>
-            <Typography
-              className="basket-item-price"
+              className="price"
               gutterBottom
               variant="h5"
               component="h2"
             >
               {product.price}
             </Typography>
-          </CardActions>
+            <Button
+              size="large"
+              className="custom-button"
+
+              // onClick={() => {
+              //   addProduct(product.id, 1);
+              // }}
+            >
+              <ShoppingCart /> Add to basket
+            </Button>
+          </>
         )}
-        <CardActions className="actions-content">
-          {!basket && (
-            <>
-              <Typography
-                className="price"
-                gutterBottom
-                variant="h5"
-                component="h2"
-              >
-                {product.price}
-              </Typography>
-              <Button
-                size="large"
-                className="custom-button"
-                
-                // onClick={() => {
-                //   addProduct(product.id, 1);
-                // }}
-              >
-                <ShoppingCart /> Add to basket
-              </Button>
-            </>
-          )}
-          {/* {basket && (
+        {/* {basket && (
             <>
               <Button
                 size="small"
@@ -105,15 +106,15 @@ import {
                   onClick={() => {
                     updateProduct(product.id, product.quantity - 1);
                   }} */}
-                {/* >
+        {/* >
                   -
                 </Button>
               </>
             </>
           )} */}
-        </CardActions>
-      </Card>
-    );
-  };
-  
-  export default CustomCard;
+      </CardActions>
+    </Card>
+  );
+};
+
+export default CustomCard;
